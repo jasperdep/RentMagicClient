226b80c363507b0185acfa8716df287075a8024f
Jasper De Pooter <jappie1999@gmail.com>
2020-05-06 14:34:00 +0200

    added oauth2 connection to Exact Online

---
+++ RentMagicClient/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OauthClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Secret()
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+
+            var serverClient = _httpClientFactory.CreateClient();
+
+            serverClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            var serverResponse = await serverClient.GetAsync("https://start.exactonline.nl/api/v1/2741128/bulk/CRM/Contacts?$top=1");
+
+            await RefreshAccessToken();
+
+            var apiClient = _httpClientFactory.CreateClient();
+
+            token = await HttpContext.GetTokenAsync("access_token");
+
+            apiClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            var apiResponse = await apiClient.GetAsync("https://localhost:5001/home/secret");
+
+            return View();
+        }
+
+        public async Task<string> RefreshAccessToken()
+        {
+            var refreshToken = await HttpContext.GetTokenAsync("refresh_token");
+
+            var refreshTokenClient = _httpClientFactory.CreateClient();
+
+            var requestData = new Dictionary<string, string>
+            {
+                ["grant_type"] = "refresh_token",
+                ["refresh_token"] = refreshToken,
+                ["client_id"] = "f458b220-7c45-45fd-a352-97a09246426d",
+                ["client_secret"] = "yJF2o6BJEqKO"
+            };
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://start.exactonline.nl/api/oauth2/token")
+            {
+                Content = new FormUrlEncodedContent(requestData)
+            };
+
+            var response = await refreshTokenClient.SendAsync(request);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseData = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseString);
+
+            var newAccessToken = responseData.GetValueOrDefault("access_token");
+            var newRefreshToken = responseData.GetValueOrDefault("refresh_token");
+
+            var authInfo = await HttpContext.AuthenticateAsync("ClientCookie");
+
+            authInfo.Properties.UpdateTokenValue("access_token", newAccessToken);
+            authInfo.Properties.UpdateTokenValue("refresh_token", newRefreshToken);
+
+            await HttpContext.SignInAsync("ClientCookie", authInfo.Principal, authInfo.Properties);
+
+            return "";
+
+        }
+    }
+}
--- RentMagicClient/Startup.cs
+++ RentMagicClient/Startup.cs
@@ -1,29 +1,60 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
 namespace RentMagicClient
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+
+            services.AddAuthentication(config => {
+                // We check the cookie to confirm that we are authenticated
+                config.DefaultAuthenticateScheme = "ClientCookie";
+                // When we sign in we will deal out a cookie
+                config.DefaultSignInScheme = "ClientCookie";
+                // use this to check if we are allowed to do something
+                config.DefaultChallengeScheme = "Exact";
+            })
+                .AddCookie("ClientCookie")
+                .AddOAuth("Unit4", config =>
+                {
+                    config.ClientId = "f0f36c11-6dd4-425d-9abe-9a69df19989f";
+                    config.ClientSecret = "2pYS7RWBY77pYDmRNBlIeV5KJZ505xfus66Nbczhxq7A12e7j9";
+                    config.CallbackPath = "/login-unit4";
+                    config.AuthorizationEndpoint = "https://sandbox.api.online.unit4.nl/V21/OAuth/Authorize";
+                    config.TokenEndpoint = "https://sandbox.api.online.unit4.nl/V21/OAuth/Token";
+
+                    config.SaveTokens = true;
+                })
+
+                .AddOAuth("Exact", config =>
+                {
+                    config.ClientId = "f458b220-7c45-45fd-a352-97a09246426d";
+                    config.ClientSecret = "yJF2o6BJEqKO";
+                    config.CallbackPath = "/login-exact";
+                    config.AuthorizationEndpoint = "https://start.exactonline.nl/api/oauth2/auth";
+                    config.TokenEndpoint = "https://start.exactonline.nl/api/oauth2/token";
+
+                    config.SaveTokens = true;
+                });
+
+            services.AddHttpClient();
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,23 +64,17 @@ namespace RentMagicClient
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
