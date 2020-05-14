cd1c027c5177cf7bb82ba83bcb0a4f9944ef7938
Jasper De Pooter <jappie1999@gmail.com>
2020-05-14 13:05:12 +0200

    refactor code for exact online integration

---
+++ RentMagicClient/Customer.cs
@@ -0,0 +1,31 @@
+using System;
+namespace RentMagicClient
+{
+    public class Customer
+    {
+        public string CustomerID { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Salutation { get; set; }
+
+        public string Street { get; set; }
+
+        public int? HouseNumber { get; set; }
+
+        public string HouseNumberAddition { get; set; }
+
+        public string Email { get; set; }
+
+        public string CountryID { get; set; }
+
+        public string LanguageID { get; set; }
+
+        public string City { get; set; }
+
+    }
+}
---
+++ RentMagicClient/ExactOnlineCustomer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace RentMagicClient
+{
+    public class ExactOnlineCustomer
+    {
+        public string ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Title { get; set; }
+
+        public string AddressStreet { get; set; }
+
+        public int? AddressStreetNumber { get; set; }
+
+        public string AddressStreetNumberSuffix { get; set; }
+
+        public string Email { get; set; }
+
+        public string Country { get; set; }
+
+        public string Language { get; set; }
+
+        public string City { get; set; }
+
+        public string AccountName { get; set; }
+
+    }
+}
