ac372a69316fbcf5c4cff1f0215340dde7e9bb3c
Jasper De Pooter <jappie1999@gmail.com>
2020-05-27 11:43:22 +0200

    show unit4 customers in view

--- RentMagicClient/Unit4Client.cs
+++ RentMagicClient/Unit4Client.cs
@@ -14,6 +14,7 @@ using Newtonsoft.Json;
 using AutoMapper;
 using Newtonsoft.Json.Linq;
 using OauthClient.Controllers;
+using RentMagicClient;
 using System.Net;
 
 namespace RentMagicClient
@@ -29,16 +30,31 @@ namespace RentMagicClient
             client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
             client.DefaultRequestHeaders.Add("Accept", "application/json");
 
-            //Rootobject container = null;
             var result = await client.GetAsync("https://sandbox.api.online.unit4.nl/V21/api/MVL71239/CustomerInfoList");
-            //if (result.IsSuccessStatusCode)
-            //{
-            //    //exactonlinecustomer = await response.Content.ReadAsAsync<ExactOnlineCustomer>();
-            //    var request = await result.Content.ReadAsStringAsync();
-            //    var rootObject = JsonConvert.DeserializeObject<RootObject>(request);
-            //    //customers = MapCustomers(rootObject.D.Results);
-            //}
+            if (result.IsSuccessStatusCode)
+            {
+                List<Unit4Customer> unit4Customers = await result.Content.ReadAsAsync<List<Unit4Customer>>();
+
+                customers = MapCustomers(unit4Customers);
+            }
             return customers;
+        }
+
+        private List<Customer> MapCustomers(List<Unit4Customer> unit4Customers)
+        {
+            return unit4Customers.ConvertAll(unit4Customers => new Customer
+            {
+                CustomerID = unit4Customers.customerId,
+                City = unit4Customers.city,
+                CompanyName = unit4Customers.name,
+                Email = unit4Customers.email,
+                HouseNumber = unit4Customers.street2,
+                HouseNumberAddition = unit4Customers.street2,
+                LanguageID = unit4Customers.languageId,
+                Street = unit4Customers.street2,
+                Tel = unit4Customers.telephone,
+                ZipCode = unit4Customers.zipCode
+            });
         }
     }
 }
--- RentMagicClient/Unit4Customer.cs
+++ RentMagicClient/Unit4Customer.cs
@@ -1,30 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace RentMagicClient
 {
     public class Unit4Customer
     {
-        public class ExactOnlineCustomer
-        {
-            public string customerId { get; set; }
+        public string customerId { get; set; }
 
-            public string fullAddress { get; set; }
+        public string street2 { get; set; }
 
-            public string email { get; set; }
+        public string email { get; set; }
 
-            public string countryId { get; set; }
+        public string countryId { get; set; }
 
-            public string languageId { get; set; }
+        public string languageId { get; set; }
 
-            public string city { get; set; }
+        public string city { get; set; }
 
-            public string name { get; set; }
+        public string name { get; set; }
 
-            public string person { get; set; }
+        public string contactPerson { get; set; }
 
-            public string telephone { get; set; }
+        public string telephone { get; set; }
 
-            public string zipCode { get; set; }
+        public string homepage { get; set; }
 
-        }
+        public string zipCode { get; set; }
+    }
+
+    public class RootUnit4
+    {
+        public List<Unit4Customer> Results { get; set; }
     }
 }
