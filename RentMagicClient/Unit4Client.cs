f9766f6d31021843ce1baee94c086cc79ac83957
Jasper De Pooter <jappie1999@gmail.com>
2020-05-27 09:17:00 +0200

    get 200 ok request on customers endpoint

---
+++ RentMagicClient/Unit4Client.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Newtonsoft.Json;
+using AutoMapper;
+using Newtonsoft.Json.Linq;
+using OauthClient.Controllers;
+using System.Net;
+
+namespace RentMagicClient
+{
+    public class Unit4Client
+    {
+        public HttpClient client = new HttpClient();
+
+        public async Task<List<Customer>> GetUnit4CustomerAsync(string path, string token)
+        {
+            List<Customer> customers = null;
+
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            //Rootobject container = null;
+            var result = await client.GetAsync("https://sandbox.api.online.unit4.nl/V21/api/MVL71239/CustomerInfoList");
+            //if (result.IsSuccessStatusCode)
+            //{
+            //    //exactonlinecustomer = await response.Content.ReadAsAsync<ExactOnlineCustomer>();
+            //    var request = await result.Content.ReadAsStringAsync();
+            //    var rootObject = JsonConvert.DeserializeObject<RootObject>(request);
+            //    //customers = MapCustomers(rootObject.D.Results);
+            //}
+            return customers;
+        }
+    }
+}
---
+++ RentMagicClient/Unit4Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace RentMagicClient
+{
+    public class Unit4Customer
+    {
+        public class ExactOnlineCustomer
+        {
+            public string customerId { get; set; }
+
+            public string fullAddress { get; set; }
+
+            public string email { get; set; }
+
+            public string countryId { get; set; }
+
+            public string languageId { get; set; }
+
+            public string city { get; set; }
+
+            public string name { get; set; }
+
+            public string person { get; set; }
+
+            public string telephone { get; set; }
+
+            public string zipCode { get; set; }
+
+        }
+    }
+}
